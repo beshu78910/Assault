1ea7fd264b3d053f8818b8c78f831449d205816e
beshu78910 <57365120+beshu78910@users.noreply.github.com>
2021-03-29 22:10:12 -0400

    Update Ship Controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class Controller : MonoBehaviour
 {
+    [Range (10,20)][SerializeField] private float speed = 20f;
+    
+    [SerializeField] private float xRange = 7f;
+
+    [SerializeField] private float yRange = 7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +22,16 @@ public class Controller : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        transform.localPosition =
-            new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+        float xOffSet = horizontal * speed * Time.deltaTime;
+        float yOffSet = vertical * speed * Time.deltaTime;
+        float rOffx = transform.localPosition.x + xOffSet;
+        float rOffy = transform.localPosition.y + yOffSet;
+        float clampPosx = Mathf.Clamp(rOffx, -xRange, xRange);
+        float clampPosy = Mathf.Clamp(rOffy, -yRange, yRange);
+        
+        transform.localPosition = new Vector3
+            (clampPosx ,
+            clampPosy, 
+            transform.localPosition.z);
     }
 }
